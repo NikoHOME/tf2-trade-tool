c7d4006f4c3fad280f4653f8e38a71d10a262a52
NikoHOME <wiktor.gawronski@gmail.com>
2023-11-24 17:56:40 +0100

    Add more descriptive comments to main program class

--- src/memory.js
+++ src/memory.js
@@ -4,25 +4,38 @@ import { Currencies } from "./metal.js";
 export class ProgramMemory
 {
     constructor() {
-        this.offer = null;
-        this.clientCurrencies = new Currencies();
-        this.ownCurrencies = new Currencies();
-        this.ownInventory;
-        this.clientInventory;
+        
 
         this.clientTradeLink;
-        this.gameAppID = 440 //Team Fortress 2
-        this.inventoryContext = 2; //Steam default
-        this.currentHistoryIndex = 0; // Command history picker
+        // Game ID of Team Fortress 2
+        this.gameAppID = 440
+        // Steam default inventory
+        // context for trading used
+        // for Steam trade manager package
+        this.inventoryContext = 2;
+        // Variable for reversing built-in 
+        // promt history to work with 
+        // own cache file
+        this.currentHistoryIndex = 0; 
         this.mySid;
 
+        // Complex classes
         this.readLine;
         this.metalManager;
         this.dealManager;
         this.tradeManager;
         this.eventEmitter;
 
+        // Current offfer variables
+        // used in dealManager
+        this.offer = null;
+        this.clientCurrencies = new Currencies();
+        this.ownCurrencies = new Currencies();
+        this.ownInventory;
+        this.clientInventory;
 
+        // Variables for creating
+        // and managing sessions
         this.token;
         this.community;
         this.user;
